3bd2769d0cc219d57db577b9e3c6176554f84257
MrPauL96 <jupvillegasgo@unal.edu.co>
2019-07-12 01:21:43 -0500

    Added modifications to calendar view

--- src/app/staff/calendar/calendar.component.ts
+++ src/app/staff/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CalendarView, CalendarEvent } from 'angular-calendar';
 @Component({
   selector: 'app-staff-calendar',
   templateUrl: './calendar.component.html',
@@ -7,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+  view: CalendarView = CalendarView.Month; //CurrentView is the actual view Month / Week
+  CalendarView = CalendarView;
+  viewDate: Date = new Date();
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+    console.log(this.viewDate);
   }
 
 }
--- src/app/staff/staff.module.ts
+++ src/app/staff/staff.module.ts
@@ -4,12 +4,18 @@ import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { SharedModule } from '../shared/shared.module';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   declarations: [DashboardComponent, CalendarComponent],
   exports: [
